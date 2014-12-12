165cda540195f74293bfddf679aa74ea020eef7c
Wartijn <martijnvansloten@gmail.com>
2014-12-12 15:04:44 +0100

    Menu gemaakt

--- PaF-Assignment-2/src/view/MainView.java
+++ PaF-Assignment-2/src/view/MainView.java
@@ -1,16 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package view;
 
-/**
- *
- * @author Rory
- */
-public class MainView {
-	///TestText
-    
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class MainView extends JFrame implements ActionListener {
+	
+	private JButton selectorButton, editorButton;
+	public MainView() {
+
+		
+        JPanel panel = new JPanel();
+        add(panel);
+		
+		selectorButton = new JButton("select pattern");
+		panel.add(selectorButton);
+		selectorButton.addActionListener(this);
+
+		editorButton = new JButton("edit pattern");
+		panel.add(editorButton);
+		editorButton.addActionListener(this);
+		
+		
+		pack();
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setTitle("Patterns!!");
+		setLocationRelativeTo(null);
+		
+	}
+	public void actionPerformed(ActionEvent event){
+		
+	}
+
 }
