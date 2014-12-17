2dce908812a7a250bdc256316d1ed9f7db607821
Chaiba <kamikaazo@hotmail.com>
2014-12-17 13:31:35 +0100

    poging controller

--- PaF-Assignment-2/src/paf/assignment/pkg2/PaFAssignment2.java
+++ PaF-Assignment-2/src/paf/assignment/pkg2/PaFAssignment2.java
@@ -12,42 +12,45 @@ import model.datahandler.Participant;
 import model.datahandler.Pattern;
 import model.datahandler.Purpose;
 import model.datahandler.Scope;
+import view.MainView;
 
 /**
  *
  * @author Rory
  */
-public class PafAssignment2 {
+public class PaFAssignment2 {
 
 	/**
 	 * @param args
 	 *            the command line arguments
 	 */
 	public static void main(String[] args) {
-		Facade f = new Facade();
-		System.out.println("Test");
-		
-		Context c1 = new Scope("scope1");
-		Context c2 = new Purpose("purpose1");
-                Pattern p = new Pattern("pName", "pProblem", "pSolution", "pConsequences", null);
-		p.addContext(c1);
-		p.addContext(c2);
-                p.addParticipant("p1Name", "p1Visibility", false, false, false);
-		p.addParticipant("p2Name", "p2Visibility", false, false, false);
-		p.getAllParticipants().get(0).addRelation("subclass", p.getAllParticipants().get(1));
-                p.getAllParticipants().get(0).addSubject("method", "methodName", "methodVisibility", false, false);
-                p.getAllParticipants().get(0).addSubject("attribute", "attributeName", "attributeVisibility", false, false);
-                
-                f.getFileHandler().exportFile("test", "objectfile", "C:\\Users\\Izak\\Desktop", p);
-                
-                f.importPattern("C:\\Users\\Izak\\Desktop\\name.obj");
-                for(Pattern ppp: f.getDataHandler().getAllPatterns()){
-                
-                    System.out.println(ppp.getName()+"sdfgv");
-                    for(Participant asd: ppp.getAllParticipants()){
-                        System.out.println(asd.getName());
-                    }
-                }
+//		Facade f = new Facade();
+//		System.out.println("Test");
+//		
+//		Context c1 = new Scope("scope1");
+//		Context c2 = new Purpose("purpose1");
+//                Pattern p = new Pattern("pName", "pProblem", "pSolution", "pConsequences", null);
+//		p.addContext(c1);
+//		p.addContext(c2);
+//                p.addParticipant("p1Name", "p1Visibility", false, false, false);
+//		p.addParticipant("p2Name", "p2Visibility", false, false, false);
+//		p.getAllParticipants().get(0).addRelation("subclass", p.getAllParticipants().get(1));
+//                p.getAllParticipants().get(0).addSubject("method", "methodName", "methodVisibility", false, false);
+//                p.getAllParticipants().get(0).addSubject("attribute", "attributeName", "attributeVisibility", false, false);
+//                
+//                f.getFileHandler().exportFile("test", "objectfile", "C:\\Users\\Izak\\Desktop", p);
+//                
+//                f.importPattern("C:\\Users\\Izak\\Desktop\\name.obj");
+//                for(Pattern ppp: f.getDataHandler().getAllPatterns()){
+//                
+//                    System.out.println(ppp.getName()+"sdfgv");
+//                    for(Participant asd: ppp.getAllParticipants()){
+//                        System.out.println(asd.getName());
+//                    }
+//                }
+            
+            MainView mv = new MainView();
 		
 	}
 
